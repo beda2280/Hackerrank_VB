0c27b5a7a9b36ab9fe0b847a3837321f4d8a80cf
Kedir Bedaso <beda2280@stthomas.edu>
2020-03-16 12:03:21 -0500

    Add project files.

---
+++ Program.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("Kedir Bedaso:  https://www.linkedin.com/in/kedir-bedaso-61555176/")
+    End Sub
+
+End Module
